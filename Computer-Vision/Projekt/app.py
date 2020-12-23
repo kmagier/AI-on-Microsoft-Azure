d078e2b10b795a421ecdc133a4c90aa50ab286f5
Karol Magier <karol.magier@gmail.com>
2020-12-23 22:28:32 +0100

    Fix

--- Computer-Vision/Projekt/app.py
+++ Computer-Vision/Projekt/app.py
@@ -1,6 +1,28 @@
-from flask import Flask
-app = Flask(__name__)
+import http.client, urllib.request, urllib.parse, urllib.error, base64
+import json
+import string
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+headers = {
+    'Content-Type': 'application/json',
+    'Prediction-Key': '<prediction-key-here>',
+}
+
+body = "{'Url' : '" + 'https://static.fajnyzwierzak.pl/media/uploads/media_image/auto/entry-content/701/desktop/nowofunland.jpg' + "' }"
+
+try:
+    conn = http.client.HTTPSConnection('westeurope.api.cognitive.microsoft.com')
+    conn.request("POST", "/customvision/v3.0/Prediction/<model-id>/classify/iterations/<iteration>/url", body, headers)
+    response = conn.getresponse()
+    data = response.read()
+    data_string = data.decode('utf-8')
+    json_data=json.loads(data_string)
+    #print(json_data)
+    if json_data:
+        for i in range(0,5):
+            value = json_data['predictions'][i]['probability']
+            race = json_data['predictions'][i]['tagName']
+            print(f'Race - {race.capitalize()} with probability {value*100}%')
+    conn.close()
+except Exception as e:
+    print("[Errno {0}] {1}".format(e.errno, e.strerror))
+    
